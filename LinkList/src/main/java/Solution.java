12d8b5289cef5cfeec71bf5e5f4692980cf6982c
DMGK <835306541@qq.com>
2024-02-29 12:11:37 +0800

    leetcode 2 两数相加

--- LinkList/src/main/java/Solution.java
+++ LinkList/src/main/java/Solution.java
@@ -1,53 +1,55 @@
 
 public class Solution {
-    private ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode h1 = headA;
-        ListNode h2 = headB;
-        while (h1 != h2) {
-            if (h1 == null) {
-                h1 = headB;
-            } else {
-                h1 = h1.next;
-            }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode head = pre;
+        int c = 0;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 == null ? 0 : l1.val;
+            int num2 = l2 == null ? 0 : l2.val;
 
-            if (h2 == null) {
-                h2 = headA;
-            } else {
-                h2 = h2.next;
-            }
+            int sum = num1 + num2 + c;
+            c = sum / 10;
+            int val = sum % 10;
+
+            ListNode newHead = new ListNode(val);
+            head.next = newHead;
+            head = head.next;
+
+
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
-        return h1;
-    }
 
+        return pre.next;
+    }
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        ListNode a1 = new ListNode(4);
-        ListNode a2 = new ListNode(1);
+        ListNode a1 = new ListNode(2);
+        ListNode a2 = new ListNode(4);
+        ListNode a3 = new ListNode(3);
         ListNode b1 = new ListNode(5);
         ListNode b2 = new ListNode(6);
-        ListNode b3 = new ListNode(1);
-        ListNode c1 = new ListNode(8);
-        ListNode c2 = new ListNode(4);
-        ListNode c3 = new ListNode(5);
+        ListNode b3 = new ListNode(4);
 
         a1.next = a2;
-        a2.next = c1;
+        a2.next = a3;
 
         b1.next = b2;
         b2.next = b3;
-        b3.next = c1;
 
-        c1.next = c2;
-        c2.next = c3;
-
-        System.out.println(s.getIntersectionNode(a1, b1).val);;
-
+        ListNode head = s.addTwoNumbers(a1, b1);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
     }
 
 }
+
 class ListNode {
     int val;
     ListNode next;
@@ -55,5 +57,13 @@ class ListNode {
     ListNode(int x) {
         val = x;
         next = null;
+    }
+
+    ListNode() {
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
