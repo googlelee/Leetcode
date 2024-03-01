b0102c1718a717ad277e13f8602c45812976765d
DMGK <835306541@qq.com>
2024-03-01 18:09:51 +0800

    leetcode100 160. 相交链表

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,25 +1,54 @@
-import java.util.*;
-
 public class Solution {
-    public void moveZeroes(int[] nums) {
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                if (i != j) { // 考虑 i=j的情况
-                    nums[j] = nums[i];
-                    nums[i] = 0;
-                }
-                j++;
-            }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+        while(a != null || b != null) {
+            if(a == b) return a;
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
-//        for (int i = 0; i < nums.length; i++) {
-//            System.out.println(nums[i]);
-//        }
+        return null;
     }
 
     public static void main(String[] args) {
+
         Solution s = new Solution();
-        int[] nums = {1};
-        s.moveZeroes(nums);
+
+        ListNode a1 = new ListNode(4);
+        ListNode a2 = new ListNode(1);
+        ListNode b1 = new ListNode(5);
+        ListNode b2 = new ListNode(6);
+        ListNode b3 = new ListNode(1);
+        ListNode c1 = new ListNode(8);
+        ListNode c2 = new ListNode(4);
+        ListNode c3 = new ListNode(5);
+
+        a1.next = a2;
+        a2.next = c1;
+
+        b1.next = b2;
+        b2.next = b3;
+        b3.next = c1;
+
+        c1.next = c2;
+        c2.next = c3;
+
+        System.out.println(s.getIntersectionNode(a1, b1).val);;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
