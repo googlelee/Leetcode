934186d17144e09b00ef7ff5ed47e3b8d57bf4b4
DMGK <835306541@qq.com>
2024-03-01 13:48:37 +0800

    leetcode 92 反转链表 II

--- LinkList/src/main/java/Solution.java
+++ LinkList/src/main/java/Solution.java
@@ -1,27 +1,38 @@
 public class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null && list2 == null) {
-            return null;
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == right) return head;
+        ListNode oldHead = head;
+        ListNode pre = null;  // 保存翻转部分前一个节点
+        int newLeft = left;
+        while (newLeft > 1) {
+            pre = head;
+            head = head.next;
+            newLeft--;
         }
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-        ListNode a = list1;
-        ListNode b = list2;
-        ListNode p = new ListNode();
-        while (a != null && b != null) {
-            if (a.val >= b.val) {
-                p.next = b;
-                b = b.next;
-            } else {
-                p.next = a;
-                a = a.next;
-            }
-            p = p.next;
-        }
-        if (a != null) p.next = a;
-        if (b != null) p.next = b;
 
-        return list1.val >= list2.val ? list2 : list1;
+        ListNode reverseTail = head;
+
+        ListNode newHead = null;
+        int len = right - left + 1;
+        while (len != 0) {
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+            len--;
+        }
+
+        if (reverseTail != null) {
+            reverseTail.next = head;
+        }
+        if (pre != null) {
+            pre.next = newHead;
+        } else {
+            pre = newHead;
+        }
+
+        if (left > 1) return oldHead; // pre已经不是头的场景
+        return pre;
     }
 
     public static void main(String[] args) {
@@ -29,27 +40,26 @@ public class Solution {
 
         ListNode a1 = new ListNode(1);
         ListNode a2 = new ListNode(2);
-        ListNode a3 = new ListNode(4);
+        ListNode a3 = new ListNode(3);
 
-        ListNode b1 = new ListNode(1);
-        ListNode b2 = new ListNode(3);
-        ListNode b3 = new ListNode(4);
+        ListNode a4 = new ListNode(4);
+        ListNode a5 = new ListNode(5);
 
 
         a1.next = a2;
         a2.next = a3;
+        a3.next = a4;
+        a4.next = a5;
 
-        b1.next = b2;
-        b2.next = b3;
 
-
-        ListNode head = s.mergeTwoLists(a1, b1);
+        ListNode head = s.reverseBetween(a1, 3, 4);
         while (head != null) {
             System.out.println(head.val);
             head = head.next;
         }
     }
 }
+
 class ListNode {
     int val;
     ListNode next;
