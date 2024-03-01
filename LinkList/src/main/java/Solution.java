e567cc165147e4c9674cbb46ed112ab1d39040bc
DMGK <835306541@qq.com>
2024-03-01 12:35:28 +0800

    leetcode 21 合并两个有序链表

--- LinkList/src/main/java/Solution.java
+++ LinkList/src/main/java/Solution.java
@@ -1,14 +1,27 @@
 public class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode newHead = null;
-        // newHead是新链表的头所以每次总是落后head一步才能成功循环
-        while (head != null) {
-            ListNode temp = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = temp;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
         }
-        return newHead;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        ListNode a = list1;
+        ListNode b = list2;
+        ListNode p = new ListNode();
+        while (a != null && b != null) {
+            if (a.val >= b.val) {
+                p.next = b;
+                b = b.next;
+            } else {
+                p.next = a;
+                a = a.next;
+            }
+            p = p.next;
+        }
+        if (a != null) p.next = a;
+        if (b != null) p.next = b;
+
+        return list1.val >= list2.val ? list2 : list1;
     }
 
     public static void main(String[] args) {
@@ -16,16 +29,21 @@ public class Solution {
 
         ListNode a1 = new ListNode(1);
         ListNode a2 = new ListNode(2);
-        ListNode a3 = new ListNode(3);
-        ListNode a4 = new ListNode(4);
-        ListNode a5 = new ListNode(5);
+        ListNode a3 = new ListNode(4);
+
+        ListNode b1 = new ListNode(1);
+        ListNode b2 = new ListNode(3);
+        ListNode b3 = new ListNode(4);
+
 
         a1.next = a2;
         a2.next = a3;
-        a3.next = a4;
-        a4.next = a5;
 
-        ListNode head = s.reverseList(a1);
+        b1.next = b2;
+        b2.next = b3;
+
+
+        ListNode head = s.mergeTwoLists(a1, b1);
         while (head != null) {
             System.out.println(head.val);
             head = head.next;
