4587c65d20370d00545dae29ff4fb7de688c2588
DMGK <835306541@qq.com>
2024-03-01 12:24:18 +0800

    leetcode 206 反转链表

--- LinkList/src/main/java/Solution.java
+++ LinkList/src/main/java/Solution.java
@@ -1,65 +1,46 @@
-
 public class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(0);
-        ListNode head = pre;
-        int c = 0;
-        while (l1 != null || l2 != null) {
-            int num1 = l1 == null ? 0 : l1.val;
-            int num2 = l2 == null ? 0 : l2.val;
-
-            int sum = num1 + num2 + c;
-            c = sum / 10;
-            int val = sum % 10;
-
-            ListNode newHead = new ListNode(val);
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        // newHead是新链表的头所以每次总是落后head一步才能成功循环
+        while (head != null) {
+            ListNode temp = head.next;
             head.next = newHead;
-            head = head.next;
-
-
-            l1 = l1 == null ? null : l1.next;
-            l2 = l2 == null ? null : l2.next;
+            newHead = head;
+            head = temp;
         }
-
-        return pre.next;
+        return newHead;
     }
 
     public static void main(String[] args) {
-
         Solution s = new Solution();
 
-        ListNode a1 = new ListNode(2);
-        ListNode a2 = new ListNode(4);
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
         ListNode a3 = new ListNode(3);
-        ListNode b1 = new ListNode(5);
-        ListNode b2 = new ListNode(6);
-        ListNode b3 = new ListNode(4);
+        ListNode a4 = new ListNode(4);
+        ListNode a5 = new ListNode(5);
 
         a1.next = a2;
         a2.next = a3;
+        a3.next = a4;
+        a4.next = a5;
 
-        b1.next = b2;
-        b2.next = b3;
-
-        ListNode head = s.addTwoNumbers(a1, b1);
+        ListNode head = s.reverseList(a1);
         while (head != null) {
             System.out.println(head.val);
             head = head.next;
         }
     }
-
 }
-
 class ListNode {
     int val;
     ListNode next;
 
-    ListNode(int x) {
-        val = x;
-        next = null;
+    ListNode() {
     }
 
-    ListNode() {
+    ListNode(int val) {
+        this.val = val;
     }
 
     ListNode(int val, ListNode next) {
