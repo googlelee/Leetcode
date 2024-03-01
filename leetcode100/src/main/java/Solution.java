825346c22bab301b22b11f8922297f0d4de89b52
DMGK <835306541@qq.com>
2024-03-01 19:34:21 +0800

    leetcode100 234. 回文链表 O(n)

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,14 +1,20 @@
 public class Solution {
-    public ListNode reverseList(ListNode head) {
-        if(head == null) return null;
-        ListNode newHead = null;
-        while(head != null) {
-            ListNode temp = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = temp;
+    public boolean isPalindrome(ListNode head) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (head != null) {
+            stringBuilder.append(head.val);
+            head = head.next;
         }
-        return newHead;
+        int i = 0, j = stringBuilder.length() - 1;
+        while (i < j) {
+            if (stringBuilder.charAt(i) == stringBuilder.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -18,19 +24,15 @@ public class Solution {
         ListNode a1 = new ListNode(4);
         ListNode a2 = new ListNode(1);
         ListNode c1 = new ListNode(8);
-        ListNode c2 = new ListNode(4);
-        ListNode c3 = new ListNode(5);
+        ListNode c2 = new ListNode(1);
+        ListNode c3 = new ListNode(4);
 
         a1.next = a2;
         a2.next = c1;
 
         c1.next = c2;
         c2.next = c3;
-        ListNode h = s.reverseList(a1);
-        while (h != null) {
-            System.out.println(h.val);
-            h = h.next;
-        }
+        System.out.println(s.isPalindrome(a1));
     }
 }
 
