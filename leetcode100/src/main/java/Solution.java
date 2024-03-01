06c68bbb806e9232f472961ab1c19e223de8dd5c
DMGK <835306541@qq.com>
2024-03-01 22:15:54 +0800

    leetcode100 142. 环形链表 II 规律题

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,35 +1,42 @@
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
+    public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
+        ListNode fast = head;
         while (fast != null) {
+            slow = slow.next;
             if (fast.next != null) {
                 fast = fast.next.next;
             } else {
-                return false;
+                return null;
             }
-            slow = slow.next;
-            if (fast == slow) return true;
+            if (slow == fast) {
+                break;
+            }
         }
-        return false;
+        if (fast == null || slow == null) return null;
+        slow = head;
+        while (fast != slow) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
     }
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        ListNode a1 = new ListNode(4);
-        ListNode a2 = new ListNode(1);
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
         ListNode c1 = new ListNode(1);
         ListNode c2 = new ListNode(4);
 //        ListNode c3 = new ListNode(4);
 
         a1.next = a2;
-        a2.next = c1;
+        a2.next = a1;
 
-        c1.next = c2;
-        c2.next = a2;
-        System.out.println(s.hasCycle(a1));
+        System.out.println(s.detectCycle(a1).val);
     }
 }
 
