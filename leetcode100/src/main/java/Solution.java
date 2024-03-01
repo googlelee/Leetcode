197a7a18c608384fd148f57654eb6596a11c49df
DMGK <835306541@qq.com>
2024-03-02 00:06:36 +0800

    leetcode100 19. 删除链表的倒数第 N 个结点

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,44 +1,38 @@
 
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int c = 0;
-        ListNode newHead = new ListNode();
-        ListNode pre = newHead;
-        while (l1 != null || l2 != null) {
-            int num1 = l1 == null ? 0 : l1.val;
-            int num2 = l2 == null ? 0 : l2.val;
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
-
-            int sum = num1 + num2 + c;
-            c = sum / 10;
-            int val = sum % 10;
-            ListNode newNode = new ListNode(val);
-            newHead.next = newNode;
-            newHead = newNode;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        // 这样方便处理删除头尾的边界情况
+        while (--n > 0) {
+            fast = fast.next;
         }
-        if (c != 0) newHead.next = new ListNode(c);
-        return pre.next;
-    }
+        if (fast.next == null) return head.next;
+        else fast = fast.next;
 
+        ListNode slow = head;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        ListNode a1 = new ListNode(2);
-        ListNode a2 = new ListNode(4);
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
         ListNode a3 = new ListNode(3);
-        ListNode b1 = new ListNode(5);
-        ListNode b2 = new ListNode(6);
-        ListNode b3 = new ListNode(4);
+        ListNode a4 = new ListNode(4);
+        ListNode a5 = new ListNode(5);
 
         a1.next = a2;
         a2.next = a3;
+        a3.next = a4;
+        a4.next = a5;
 
-        b1.next = b2;
-        b2.next = b3;
-
-        ListNode head = s.addTwoNumbers(a1, b1);
+        ListNode head = s.removeNthFromEnd(a1, 1);
         while (head != null) {
             System.out.println(head.val);
             head = head.next;
