001b894d8a11d79dfa717ee0a933fcabe64521a3
DMGK <835306541@qq.com>
2024-03-01 20:33:57 +0800

    leetcode100 234. 回文链表 O(1)

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,20 +1,34 @@
 public class Solution {
     public boolean isPalindrome(ListNode head) {
-        StringBuilder stringBuilder = new StringBuilder();
-        while (head != null) {
-            stringBuilder.append(head.val);
+        // 找到中点
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode newHead = reverse(slow.next);
+        slow.next = null;
+        while (head != null && newHead != null) {
+            if (head.val != newHead.val) return false;
             head = head.next;
+            newHead = newHead.next;
         }
-        int i = 0, j = stringBuilder.length() - 1;
-        while (i < j) {
-            if (stringBuilder.charAt(i) == stringBuilder.charAt(j)) {
-                i++;
-                j--;
-            } else {
-                return false;
-            }
-        }
+        if (head != null) return head.next == null;
+        if (newHead != null) return newHead.next == null;
         return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+        }
+        return newHead;
     }
 
     public static void main(String[] args) {
@@ -23,15 +37,15 @@ public class Solution {
 
         ListNode a1 = new ListNode(4);
         ListNode a2 = new ListNode(1);
-        ListNode c1 = new ListNode(8);
-        ListNode c2 = new ListNode(1);
-        ListNode c3 = new ListNode(4);
+        ListNode c1 = new ListNode(1);
+        ListNode c2 = new ListNode(4);
+//        ListNode c3 = new ListNode(4);
 
         a1.next = a2;
         a2.next = c1;
 
         c1.next = c2;
-        c2.next = c3;
+//        c2.next = c3;
         System.out.println(s.isPalindrome(a1));
     }
 }
