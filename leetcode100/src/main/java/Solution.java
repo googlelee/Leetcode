4fb239e64f0a4d1d1d37b4299d0fba85ec2955f6
DMGK <835306541@qq.com>
2024-03-02 08:47:20 +0800

    leetcode100 35. 搜索插入位置

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,58 +1,23 @@
 
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head;
-        // 这样方便处理删除头尾的边界情况
-        while (--n > 0) {
-            fast = fast.next;
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left != right) {
+            if (target > nums[left]) {
+                left++;
+            } else {
+                right--;
+            }
         }
-        if (fast.next == null) return head.next;
-        else fast = fast.next;
-
-        ListNode slow = head;
-        while (fast.next != null) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-        slow.next = slow.next.next;
-        return head;
+        if (nums[left] < target) left++;
+        return left;
     }
+
     public static void main(String[] args) {
 
         Solution s = new Solution();
+        int[] nums = new int[] {1,3,5,6};
 
-        ListNode a1 = new ListNode(1);
-        ListNode a2 = new ListNode(2);
-        ListNode a3 = new ListNode(3);
-        ListNode a4 = new ListNode(4);
-        ListNode a5 = new ListNode(5);
-
-        a1.next = a2;
-        a2.next = a3;
-        a3.next = a4;
-        a4.next = a5;
-
-        ListNode head = s.removeNthFromEnd(a1, 1);
-        while (head != null) {
-            System.out.println(head.val);
-            head = head.next;
-        }
-    }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+        System.out.println(s.searchInsert(nums, 5));
     }
 }
