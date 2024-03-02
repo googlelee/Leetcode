2981e5ab46ff8bed886419a6be949e4b257ce055
DMGK <835306541@qq.com>
2024-03-02 09:57:44 +0800

    leetcode100 118. 杨辉三角

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,19 +1,35 @@
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public int climbStairs(int n) {
-        if (n == 1) return 1; // 边界
-        int[] cnts = new int[n + 1];
-        cnts[1] = 1;
-        cnts[2] = 2;
-        for (int i = 3; i < n + 1; i++) {
-            cnts[i] = cnts[i - 1] + cnts[i - 2];
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> first = new ArrayList<>();
+        first.add(1);
+        res.add(first);
+        if (numRows == 1) return res;
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> lastRow = res.getLast();
+
+            for (int j = 0; j < lastRow.size(); j++) {
+                int num;
+                if (j - 1 >= 0) {
+                    num = lastRow.get(j) + lastRow.get(j - 1);
+                } else {
+                    num = lastRow.get(j);
+                }
+                row.add(num);
+            }
+            row.add(1);
+            res.add(row);
         }
-        return cnts[n];
+        return res;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.climbStairs(3));
+        System.out.println(s.generate(5));
     }
 }
