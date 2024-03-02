2ac5c0206762e0b2a170b34f8a5bd6fc664ac9cb
DMGK <835306541@qq.com>
2024-03-02 13:28:34 +0800

    leetcode100 15. 三数之和

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,26 +1,40 @@
+import java.util.*;
+
 class Solution {
-    public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int res = 0;
-        while (left < right) {
-            int h = Math.min(height[left], height[right]);
-            int area = h * (right - left);
-            if (area > res) {
-                res = area;
-            } else {
-                if (height[left] >= height[right]) {
-                    right--;
-                } else {
-                    left++;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set<String> seen = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            // 剪枝 否则超时
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int target = -nums[i];
+            Map<Integer, Integer> hash = new HashMap<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                if (hash.containsKey(target - nums[j])) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(target - nums[j]);
+                    list.add(nums[j]);
+
+                    // 去重
+                    String combination = list.toString();
+                    if (!seen.contains(combination)) { // 复杂度 O(1)
+                        res.add(list);
+                        seen.add(combination);
+                    }
                 }
+                hash.put(nums[j], 1);
             }
         }
         return res;
     }
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] heights = new int[]{1,8,6,2,5,4,8,3,7};
-        System.out.println(s.maxArea(heights));
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
+        System.out.println(s.threeSum(nums));
     }
 }
