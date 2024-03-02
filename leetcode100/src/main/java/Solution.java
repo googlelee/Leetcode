0e00e23bfac538c1cac5e10dc18ac17391368590
DMGK <835306541@qq.com>
2024-03-02 22:12:20 +0800

    leetcode100 73. 矩阵置零

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,40 +1,50 @@
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Set<String> seen = new HashSet<>();
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            // 剪枝 否则超时
-            if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int target = -nums[i];
-            Map<Integer, Integer> hash = new HashMap<>();
-            for (int j = i + 1; j < nums.length; j++) {
-                if (hash.containsKey(target - nums[j])) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(target - nums[j]);
-                    list.add(nums[j]);
-
-                    // 去重
-                    String combination = list.toString();
-                    if (!seen.contains(combination)) { // 复杂度 O(1)
-                        res.add(list);
-                        seen.add(combination);
+    public void setZeroes(int[][] matrix) {
+        List<Integer> rows = new ArrayList<>();
+        List<Integer> cols = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (!rows.contains(i)) {
+                        rows.add(i);
+                    }
+                    if (!cols.contains(j)) {
+                        cols.add(j);
                     }
                 }
-                hash.put(nums[j], 1);
             }
         }
-        return res;
+
+        for (Integer row : rows) {
+            setRowZero(matrix, row);
+        }
+
+        for (Integer col : cols) {
+            setColZero(matrix, col);
+        }
+
+    }
+
+    private void setColZero(int[][] matrix, int j) {
+        int rows = matrix.length;
+        for (int i = 0; i < rows; i++) {
+            matrix[i][j] = 0;
+        }
+    }
+
+    private void setRowZero(int[][] matrix, int i) {
+        int cols = matrix[0].length;
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = 0;
+        }
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = new int[]{-1,0,1,2,-1,-4};
-        System.out.println(s.threeSum(nums));
+        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        s.setZeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
