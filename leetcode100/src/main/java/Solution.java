f6b1e7b3c327462ff8667abab1e1098cec8307a3
DMGK <835306541@qq.com>
2024-03-03 12:08:55 +0800

    leetcode100 53. 最大子数组和

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,14 +1,26 @@
 import java.util.*;
 
 class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+    public int maxSubArray(int[] nums) {
+        int[] maxSum = new int[nums.length];
+        maxSum[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] + maxSum[i - 1] > nums[i]) {
+                maxSum[i] = nums[i] + maxSum[i - 1];
+            } else {
+                maxSum[i] = nums[i];
+            }
+            if (maxSum[i] > res) {
+                res = maxSum[i];
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {2,2,1,1,1,2,2};
-        System.out.println(s.majorityElement(nums));
+        int[] nums = {5,4,-1,7,8};
+        System.out.println(s.maxSubArray(nums));
     }
 }
