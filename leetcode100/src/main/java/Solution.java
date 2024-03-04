5c34504a969e0e2d631d86aefec95cc21dc83aa8
DMGK <835306541@qq.com>
2024-03-04 16:24:02 +0800

    leetcode100 94. 二叉树的中序遍历

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,22 +1,45 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public boolean canJump(int[] nums) {
-        if (nums.length == 1) return true;
-        int max = 0; // 维护当前位置上能达到的最远下标
-        for (int i = 0; i < nums.length; i++) {
-            if (max < i) return false; // 到达不了此处则false
-            max = Math.max(max, i + nums[i]);
-            if (max >= nums.length - 1) return true; // 可以到达没必要继续遍历
-        }
-        return true;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
     }
+
+    private void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        int[] nums = {3,2,1,0,4};
-        System.out.println(s.canJump(nums));
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+
+        a.right = b;
+        b.left = c;
+
+        System.out.println(s.inorderTraversal(a));
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
