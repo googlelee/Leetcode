df0dab8018fbeb6bab8d28b395d8f854a5e33a49
DMGK <835306541@qq.com>
2024-03-04 10:47:13 +0800

    leetcode100 34. 在排序数组中查找元素的第一个和最后一个位置

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,38 +1,42 @@
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int left = 0, right = rows;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (target > matrix[mid][cols - 1]) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        if (left >= rows) return false;
-        int row = left;
-        left = 0;
-        right = cols;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (target > matrix[row][mid]) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        if (left >= cols) return false;
-        return matrix[row][left] == target;
-    }
+import java.util.Arrays;
 
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[]{-1, -1};
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            // 这儿是解决死循环的绝招  left 1 right 2 为例即可理解
+            int mid = (left + right + 1) / 2;
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        if (right >= 0 && nums[right] == target) {
+            res[1] = right;
+        }
+        left = 0;
+        right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        if (right >= 0 && nums[right] == target) {
+            res[0] = right;
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        int[][] matrix = {{1}};
-        System.out.println(s.searchMatrix(matrix, 1));
+        int[] nums = {5,7,7,8,8,10};
+        System.out.println(Arrays.toString(s.searchRange(nums, 10)));
     }
 }
