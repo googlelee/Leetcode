149bab08604760e19b1ada2714d16999b269ccdb
DMGK <835306541@qq.com>
2024-03-04 08:24:29 +0800

    leetcode100 24. 两两交换链表中的节点

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,26 +1,84 @@
 import java.util.*;
 
 class Solution {
-    public int maxSubArray(int[] nums) {
-        int[] maxSum = new int[nums.length];
-        maxSum[0] = nums[0];
-        int res = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] + maxSum[i - 1] > nums[i]) {
-                maxSum[i] = nums[i] + maxSum[i - 1];
+    public ListNode swapPairs(ListNode head) {
+        List<ListNode> odd = new ArrayList<>();
+        List<ListNode> even = new ArrayList<>();
+        int cnt = 0;
+        while (head != null) {
+            if (cnt % 2 == 0) {
+                odd.add(head);
             } else {
-                maxSum[i] = nums[i];
+                even.add(head);
             }
-            if (maxSum[i] > res) {
-                res = maxSum[i];
-            }
+            head = head.next;
+            cnt++;
         }
-        return res;
+        ListNode p = new ListNode();
+        ListNode newHead = p;
+        int i = 0, j = 0;
+        int s = 0;
+        while (s < cnt) {
+            if (s % 2 != 0 && i < odd.size()) {
+                odd.get(i).next = null;
+                p.next = odd.get(i++);
+                p = p.next;
+            }
+            if (s % 2 == 0 && j < even.size()){
+                even.get(j).next = null;
+                p.next = even.get(j++);
+                p = p.next;
+            }
+            s++;
+        }
+        while (i < odd.size()) {
+            odd.get(i).next = null;
+            p.next = odd.get(i++);
+        }
+        while (j < even.size()) {
+            odd.get(j).next = null;
+            p.next = even.get(j++);
+        }
+        return newHead.next;
     }
 
     public static void main(String[] args) {
+
         Solution s = new Solution();
-        int[] nums = {5,4,-1,7,8};
-        System.out.println(s.maxSubArray(nums));
+
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
+        ListNode a3 = new ListNode(3);
+        ListNode a4 = new ListNode(4);
+        ListNode a5 = new ListNode(5);
+
+        a1.next = a2;
+        a2.next = a3;
+        a3.next = a4;
+        a4.next = a5;
+
+        ListNode head = s.swapPairs(a1);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
     }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
 }
