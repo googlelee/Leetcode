3cb7250b44543da904f848b33e5c9a2a997b40e6
DMGK <835306541@qq.com>
2024-03-04 20:44:01 +0800

    leetcode100 101. 对称二叉树

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,55 +1,65 @@
+import java.lang.management.BufferPoolMXBean;
 import java.util.*;
 
 class Solution {
     // dfs
-    // public TreeNode invertTree(TreeNode root) {
-    //     if (root == null) return null;
-    //     TreeNode temp = root.right;
-    //     root.right = root.left;
-    //     root.left= temp;
-    //     invertTree(root.left);
-    //     invertTree(root.right);
-    //     return root;
-    // }
-
-    // bfs
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) return null;
-        List<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            TreeNode front = queue.removeFirst();
-            TreeNode temp = front.left;
-            front.left = front.right;
-            front.right = temp;
-            if (front.left != null) {
-                queue.add(front.left);
-            }
-            if (front.right != null) {
-                queue.add(front.right);
-            }
-        }
-        return root;
+    public boolean isSymmetric(TreeNode root) {
+        if (root.left == null && root.right == null) return true;
+        return isSymmetricBoth(root.left, root.right);
     }
 
+    private boolean isSymmetricBoth(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isSymmetricBoth(left.left, right.right) && isSymmetricBoth(left.right, right.left);
+    }
 
+    // bfs
+    // public boolean isSymmetric(TreeNode root) {
+    //     if (root.left == null && root.right == null) return true;
+    //     List<TreeNode> queue = new LinkedList<>();
+    //     queue.add(root.left);
+    //     queue.add(root.right);
+    //     while (!queue.isEmpty()) {
+    //         TreeNode left = queue.removeFirst();
+    //         TreeNode right = queue.removeFirst();
+    //         if (left == null && right == null) continue;
+    //         if (left == null || right == null) return false;
+    //         if (left.val != right.val) return false;
+    //         queue.add(left.left);
+    //         queue.add(right.right);
+    //         queue.add(left.right);
+    //         queue.add(right.left);
+    //     }
+    //     return true;
+    // }
 
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        TreeNode a = new TreeNode(2);
-        TreeNode b = new TreeNode(3);
-        TreeNode c = new TreeNode(1);
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(2);
+        TreeNode d = new TreeNode(2);
+        TreeNode e = new TreeNode(2);
+        TreeNode f = new TreeNode(4);
+        TreeNode g = new TreeNode(3);
 
-        a.right = b;
-        a.left = c;
 
-        TreeNode root = s.invertTree(a);
-        List<Integer> res = new LinkedList<>();
-        frontOrder(root, res);
-        System.out.println(res);
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        // b.right = e;
+        c.left = e;
+        // c.right = g;
+
+        // TreeNode root = s.invertTree(a);
+        // List<Integer> res = new LinkedList<>();
+        // frontOrder(root, res);
+        System.out.println(s.isSymmetric(a));
     }
 
     private static void frontOrder(TreeNode root, List<Integer> res) {
