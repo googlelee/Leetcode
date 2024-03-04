62ffc7a7de13c48fc55d6b5d439c1c2951215f19
DMGK <835306541@qq.com>
2024-03-04 12:14:18 +0800

    leetcode100 739. 每日温度

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,33 +1,39 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[]{-1, -1};
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            // 这儿是解决死循环的绝招  left 1 right 2 为例即可理解
-            int mid = (left + right + 1) / 2;
-            if (target < nums[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid;
+//    public int[] dailyTemperatures(int[] temperatures) {
+//        int[] res = new int[temperatures.length];
+//        Stack<List<Integer>> stack = new Stack<>();
+//        for (int i = 0; i < temperatures.length; i++) {
+//            if (stack.isEmpty() || stack.peek().get(1) >= temperatures[i]) {
+//                List<Integer> list = new ArrayList<>();
+//                list.add(i);
+//                list.add(temperatures[i]);
+//                stack.push(list);
+//            } else {
+//                while (!stack.isEmpty() && stack.peek().get(1) < temperatures[i]) {
+//                    res[stack.peek().getFirst()] = i - stack.peek().getFirst();
+//                    stack.pop();
+//                }
+//                List<Integer> list = new ArrayList<>();
+//                list.add(i);
+//                list.add(temperatures[i]);
+//                stack.push(list);
+//            }
+//        }
+//        res[temperatures.length - 1] = 0;
+//        return res;
+//    }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                res[stack.peek()] = i - stack.peek();
+                stack.pop();
             }
-        }
-        if (right >= 0 && nums[right] == target) {
-            res[1] = right;
-        }
-        left = 0;
-        right = nums.length - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (target > nums[mid]) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        if (right >= 0 && nums[right] == target) {
-            res[0] = right;
+            stack.push(i);
         }
         return res;
     }
@@ -36,7 +42,7 @@ class Solution {
 
         Solution s = new Solution();
 
-        int[] nums = {5,7,7,8,8,10};
-        System.out.println(Arrays.toString(s.searchRange(nums, 10)));
+        int[] nums = {73,74,75,71,69,72,76,73};
+        System.out.println(Arrays.toString(s.dailyTemperatures(nums)));
     }
 }
