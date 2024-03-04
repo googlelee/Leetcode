b9f6492dd7e154c2a010057be99eeb3f3a18194b
DMGK <835306541@qq.com>
2024-03-04 13:22:16 +0800

    leetcode100 347. 前 K 个高频元素

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,34 +1,48 @@
 import java.util.*;
 
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        // 大顶堆
-        // PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
-        //     @Override
-        //     public int compare(Integer o1, Integer o2) {
-        //         return o2 - o1;
-        //     }
-        // });
-
-        // 默认为小顶堆
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (i < k) priorityQueue.add(nums[i]);
-            else {
-                if (priorityQueue.peek() < nums[i]) {
-                    priorityQueue.poll();
-                    priorityQueue.add(nums[i]);
-                }
+            if (hashMap.containsKey(nums[i])) {
+                hashMap.put(nums[i], hashMap.get(nums[i]) + 1);
+            } else {
+                hashMap.put(nums[i], 1);
             }
         }
-        return priorityQueue.peek();
+        int[] res = new int[k];
+        PriorityQueue<List<Integer>> priorityQueue = new PriorityQueue<>(new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                return o1.getLast() - o2.getLast();
+            }
+        });
+        for (Integer num : hashMap.keySet()) {
+            if (priorityQueue.size() < k) {
+                List<Integer> list = new ArrayList<>();
+                list.add(num);
+                list.add(hashMap.get(num));
+                priorityQueue.add(list);
+            } else if (hashMap.get(num) > priorityQueue.peek().getLast()){
+                priorityQueue.poll();
+                List<Integer> list1 = new ArrayList<>();
+                list1.add(num);
+                list1.add(hashMap.get(num));
+                priorityQueue.add(list1);
+            }
+        }
+        int i = 0;
+        while (!priorityQueue.isEmpty()) {
+            res[i++] = priorityQueue.poll().getFirst();
+        }
+        return res;
     }
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        int[] nums = {3, 2, 3, 1, 2, 4, 5, 5, 6};
-        System.out.println(s.findKthLargest(nums, 4));
+        int[] nums = {1,1,1,2,2,3};
+        System.out.println(Arrays.toString(s.topKFrequent(nums, 2)));
     }
 }
