255ce9cd94c95bb6642ffddfe61e144736b1b11e
DMGK <835306541@qq.com>
2024-03-04 14:35:17 +0800

    leetcode100 55. 跳跃游戏

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,42 +1,22 @@
 import java.util.*;
 
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> hashMap = new HashMap<>();
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) return true;
+        int max = 0; // 维护当前位置上能达到的最远下标
         for (int i = 0; i < nums.length; i++) {
-            if (hashMap.containsKey(nums[i])) {
-                hashMap.put(nums[i], hashMap.get(nums[i]) + 1);
-            } else {
-                hashMap.put(nums[i], 1);
-            }
+            if (max < i) return false; // 到达不了此处则false
+            max = Math.max(max, i + nums[i]);
+            if (max >= nums.length - 1) return true; // 可以到达没必要继续遍历
         }
-        int[] res = new int[k];
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return hashMap.get(o1) - hashMap.get(o2);
-            }
-        });
-        for (Integer num : hashMap.keySet()) {
-            if (priorityQueue.size() < k) {
-                priorityQueue.add(num);
-            } else if (hashMap.get(num) > hashMap.get(priorityQueue.peek())){
-                priorityQueue.poll();
-                priorityQueue.add(num);
-            }
-        }
-        int i = 0;
-        while (!priorityQueue.isEmpty()) {
-            res[i++] = priorityQueue.poll();
-        }
-        return res;
+        return true;
     }
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        int[] nums = {4,1,-1,2,-1,2,3};
-        System.out.println(Arrays.toString(s.topKFrequent(nums, 2)));
+        int[] nums = {3,2,1,0,4};
+        System.out.println(s.canJump(nums));
     }
 }
