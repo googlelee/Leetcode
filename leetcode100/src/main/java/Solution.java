83fb3b5fbc28bc06a54014d2249f3e79e6c148cc
DMGK <835306541@qq.com>
2024-03-04 10:02:41 +0800

    leetcode100 74. 搜索二维矩阵

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,20 +1,30 @@
-import java.lang.management.BufferPoolMXBean;
-import java.util.*;
-
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        ListNode pre = new ListNode(0);
-        pre.next = head;
-        ListNode temp = pre;
-        while (temp.next != null && temp.next.next != null) {
-            ListNode start = temp.next;
-            ListNode end = temp.next.next;
-            temp.next = end;
-            start.next = end.next;
-            end.next = start;
-            temp = start; // 这一步很重要 容易晕
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left = 0, right = rows;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (target > matrix[mid][cols - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
-        return pre.next;
+        if (left >= rows) return false;
+        int row = left;
+        left = 0;
+        right = cols;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (target > matrix[row][mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        if (left >= cols) return false;
+        return matrix[row][left] == target;
     }
 
 
@@ -22,39 +32,7 @@ class Solution {
 
         Solution s = new Solution();
 
-        ListNode a1 = new ListNode(1);
-        ListNode a2 = new ListNode(2);
-        ListNode a3 = new ListNode(3);
-        ListNode a4 = new ListNode(4);
-        ListNode a5 = new ListNode(5);
-
-        a1.next = a2;
-        a2.next = a3;
-        a3.next = a4;
-        a4.next = a5;
-
-        ListNode head = s.swapPairs(a1);
-        while (head != null) {
-            System.out.println(head.val);
-            head = head.next;
-        }
+        int[][] matrix = {{1}};
+        System.out.println(s.searchMatrix(matrix, 1));
     }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-
 }
