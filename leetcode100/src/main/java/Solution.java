12a186ae50ed4882d37d2c654600c31b44f3b53a
DMGK <835306541@qq.com>
2024-03-05 16:36:57 +0800

    leetcode100 64. 最小路径和

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,53 +1,31 @@
 import java.util.ArrayList;
+import java.util.concurrent.ForkJoinPool;
 import java.util.jar.JarEntry;
 
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int i, j = 0;
-        // 特殊处理
-        if (nums.length == 1) return;
-        if (nums.length == 2) {
-            swapNums(nums, 0, 1);
-            return;
+    public int minPathSum(int[][] grid) {
+        int[][] cost = new int[grid.length][grid[0].length];
+        cost[0][0] = grid[0][0];
+        for (int i = 1; i < grid.length; i++) {
+            cost[i][0] = cost[i - 1][0] + grid[i][0];
         }
-        // 找到从尾到头第一个升序对 记为 i j
-        for (i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                j = i + 1;
-                break;
+        for (int j = 1; j < grid[0].length; j++) {
+            cost[0][j] = cost[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                cost[i][j] = grid[i][j] + Math.min(cost[i - 1][j], cost[i][j - 1]);
             }
         }
-        // 如果成功 则从nums.length - 1到j找第一个大于num[i]的位置并交换两者
-        if (i != -1) { // 未找到证明整个序列是降序 直接翻转即可
-            for (int k = nums.length - 1; k >= j; k--) {
-                if (nums[k] > nums[i]) {
-                    swapNums(nums, k, i);
-                    break;
-                }
-            }
-        }
-        // 此时j到nums.length - 1是降序 转为升序
-        int start = j, end = nums.length - 1;
-        while (start < end) {
-            swapNums(nums, start, end);
-            start++;
-            end--;
-        }
-    }
-
-    private void swapNums(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        return cost[cost.length - 1][cost[0].length - 1];
     }
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        // int[] nums = {1, 2, 3, 8, 5, 7, 6, 4};
-        int[] nums = {3,2,1};
-        s.nextPermutation(nums);
-        for (int num : nums) System.out.print(num);
+        int[][] nums = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        // int[][] nums = {{1}};
+        System.out.println(s.minPathSum(nums));
     }
 }
