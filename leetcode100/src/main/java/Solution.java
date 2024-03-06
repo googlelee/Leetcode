d21baab148df25c334a3467b1fc8a67848c88d9b
DMGK <835306541@qq.com>
2024-03-06 20:51:43 +0800

    leetcode100 300. 最长递增子序列

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -6,41 +6,26 @@ import java.util.jar.JarEntry;
 
 class Solution {
 
-    public int coinChange(int[] coins, int amount) {
-        int[] cnt = new int[amount + 1];
-        cnt[0] = 0;
-        Arrays.sort(coins);
-        for (int i = 1; i < cnt.length; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                // 一张就可以满足
-                if (i - coins[j] == 0) {
-                    cnt[i] = 1;
-                } else if (i - coins[j] > 0) {
-                    // 可以构成amount的情况
-                    if (cnt[i - coins[j]] > 0) {
-                        // 有多种构成i的情况下选择最小的
-                        if (cnt[i] > 0) {
-                            cnt[i] = Math.min(cnt[i - coins[j]] + 1, cnt[i]);
-                        } else {
-                            cnt[i] = cnt[i - coins[j]] + 1;
-                        }
-                    } else {
-                        // 防止覆盖
-                        if (cnt[i] <= 0) cnt[i] = -1;
-                    }
-                } else if (i - coins[j] < 0) {
-                    // 防止覆盖
-                    if (cnt[i] <= 0) cnt[i] = -1;
+    public int lengthOfLIS(int[] nums) {
+        int[] cnt = new int[nums.length];
+        cnt[0] = 1;
+        int max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            cnt[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                // 必须遍历完0到i-1找到最大
+                if (nums[j] < nums[i]) {
+                    cnt[i] = Math.max(cnt[j] + 1, cnt[i]);
+                    if (max < cnt[i]) max = cnt[i];
                 }
             }
         }
-
-        return cnt[amount];
+        return max;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {186, 419, 83, 408};
-        System.out.println(s.coinChange(nums, 6249));
+        int[] nums = {0, 1, 0, 3, 2, 3};
+        System.out.println(s.lengthOfLIS(nums));
     }
 }
