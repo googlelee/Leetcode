6898f463b5b361679eed5ebc737d673a2ad574eb
DMGK <835306541@qq.com>
2024-03-07 09:04:07 +0800

    leetcode100 300. 最长递增子序列 o(nlogn)

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,31 +1,41 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.NavigableMap;
-import java.util.concurrent.ForkJoinPool;
-import java.util.jar.JarEntry;
+import java.util.Stack;
 
 class Solution {
 
     public int lengthOfLIS(int[] nums) {
-        int[] cnt = new int[nums.length];
-        cnt[0] = 1;
-        int max = 1;
-        for (int i = 1; i < nums.length; i++) {
-            cnt[i] = 1;
-            for (int j = i - 1; j >= 0; j--) {
-                // 必须遍历完0到i-1找到最大
-                if (nums[j] < nums[i]) {
-                    cnt[i] = Math.max(cnt[j] + 1, cnt[i]);
-                    if (max < cnt[i]) max = cnt[i];
-                }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (stack.isEmpty()) {
+                stack.add(nums[i]);
+            } else if (nums[i] < stack.peek()) {
+                // 为什么只换相应位置就可以？
+                // 因为换相应位置不会影响已经获得的最大长度  即栈的size
+                // 又达到了保存该长度最小尾部的效果  从而可以进一步搜索是否有更长的序列
+                int pos = findPos(stack, nums[i]);
+                stack.set(pos, nums[i]);
+            } else if (nums[i] > stack.peek()) {
+                stack.push(nums[i]);
             }
         }
-        return max;
+        return stack.size();
+    }
+
+    private int findPos(Stack<Integer> stack, int target) {
+        int left = 0, right = stack.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (target > stack.get(mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {0, 1, 0, 3, 2, 3};
+        int[] nums = {10, 9, 2, 5, 1, 7, 101, 18};
         System.out.println(s.lengthOfLIS(nums));
     }
 }
