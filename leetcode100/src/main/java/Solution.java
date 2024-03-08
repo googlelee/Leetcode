39cb8573a5e92db8698efb25f00aa5925632c491
DMGK <835306541@qq.com>
2024-03-08 09:36:53 +0800

    leetcode100 416. 分割等和子集

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -2,26 +2,31 @@ import java.util.Stack;
 
 class Solution {
 
-    public int maxProduct(int[] nums) {
-        // max[i]存以i为结尾最大的子序列乘积
-        // min[i]存以i为结尾最小的子序列乘积
-        int[] max = new int[nums.length];
-        int[] min = new int[nums.length];
-        // max在出现max出现负数时断  继续从当前位置开始
-        // min在出现min出现整数时断  继续从当前位置开始
-        int res = max[0] = min[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            // max和min其实只需要用一个变量就可以 这里是为了方便看过程用数组
-            max[i] = Math.max(nums[i], Math.max(max[i - 1] * nums[i], min[i - 1] * nums[i]));
-            min[i] = Math.min(nums[i], Math.min(max[i - 1] * nums[i], min[i - 1] * nums[i]));
-            if (res < max[i]) res = max[i];
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
-        return res;
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        boolean[][] cnt = new boolean[nums.length][target + 1];
+        if (nums[0] <= target) { // 边界
+            cnt[0][nums[0]] = true;
+        }
+        // cnt[i][j]代表0到i这i+1个元素能否构成target 经典背包问题
+        for (int i = 1; i < cnt.length; i++) {
+            for (int j = 0; j < cnt[0].length; j++) {
+                if (j < nums[i]) cnt[i][j] = cnt[i - 1][j];
+                else if (j == nums[i]) cnt[i][j] = true;
+                else cnt[i][j] = cnt[i - 1][j] || cnt[i - 1][j - nums[i]];
+            }
+        }
+        return cnt[nums.length - 1][target];
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {2,3,-2,4};
-        System.out.println(s.maxProduct(nums));
+        int[] nums = {100};
+        System.out.println(s.canPartition(nums));
     }
 }
