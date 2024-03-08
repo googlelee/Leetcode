90842daa8d6cfcbd5020f5e62e600988b0ffb73a
DMGK <835306541@qq.com>
2024-03-08 17:56:27 +0800

    leetcode100 238. 除自身以外数组的乘积

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -2,42 +2,29 @@ import java.util.*;
 
 class Solution {
 
-    // public void rotate(int[] nums, int k) {
-    //     int len = nums.length;
-    //     k = k % len;
-    //     int[] tmp = new int[nums.length];
-    //     for (int i = 0; i < len; i++) {
-    //         if (len - k + i < len) {
-    //             tmp[i] = nums[len - k + i];
-    //         } else {
-    //             tmp[i] = nums[(len - k + i) % len];
-    //         }
-    //     }
-    //     System.arraycopy(tmp, 0, nums, 0, len);
-    // }
-
-    public void rotate(int[] nums, int k) {
-        int len = nums.length;
-        k = k % len;
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+    public int[] productExceptSelf(int[] nums) {
+        int[] leftRight = new int[nums.length];
+        int[] rightLeft = new int[nums.length];
+        leftRight[0] = nums[0];
+        rightLeft[nums.length - 1] = nums[nums.length - 1];
+        for (int i = 1; i < nums.length; i++) {
+            leftRight[i] = leftRight[i - 1] * nums[i];
         }
+        for (int i = nums.length - 2; i >= 0 ; i--) {
+            rightLeft[i] = rightLeft[i + 1] * nums[i];
+        }
+        int[] res = new int[nums.length];
+        res[0] = rightLeft[1];
+        res[nums.length - 1] = leftRight[nums.length - 2];
+        for (int i = 1; i < nums.length - 1; i++) {
+            res[i] = leftRight[i - 1] * rightLeft[i + 1];
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {1,2,3,4,5,6,7};
-        s.rotate(nums, 3);
-        System.out.println(Arrays.toString(nums));
+        int[] nums = {1,2};
+        System.out.println(Arrays.toString(s.productExceptSelf(nums)));
     }
 }
