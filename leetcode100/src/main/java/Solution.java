3de1bd3a8ddb6adcf637164e7c25696b0d3225a3
DMGK <835306541@qq.com>
2024-03-08 10:36:33 +0800

    leetcode100 49. 字母异位词分组

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,32 +1,23 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
 
-    public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> hashMap = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] tmp = strs[i].toCharArray();
+            Arrays.sort(tmp);
+            String key = new String(tmp);
+            List<String> ss = hashMap.getOrDefault(key, new ArrayList<>());
+            ss.add(strs[i]);
+            hashMap.put(key, ss);
         }
-        if (sum % 2 != 0) return false;
-        int target = sum / 2;
-        boolean[][] cnt = new boolean[nums.length][target + 1];
-        if (nums[0] <= target) { // 边界
-            cnt[0][nums[0]] = true;
-        }
-        // cnt[i][j]代表0到i这i+1个元素能否构成target 经典背包问题
-        for (int i = 1; i < cnt.length; i++) {
-            for (int j = 0; j < cnt[0].length; j++) {
-                if (j < nums[i]) cnt[i][j] = cnt[i - 1][j];
-                else if (j == nums[i]) cnt[i][j] = true;
-                else cnt[i][j] = cnt[i - 1][j] || cnt[i - 1][j - nums[i]];
-            }
-        }
-        return cnt[nums.length - 1][target];
+        return new ArrayList<>(hashMap.values());
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {100};
-        System.out.println(s.canPartition(nums));
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(s.groupAnagrams(strs));
     }
 }
