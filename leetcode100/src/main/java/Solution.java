1943c321072082ce2fa6ad7cc2f20b7040d47aea
DMGK <835306541@qq.com>
2024-03-08 21:21:49 +0800

    leetcode100 48. 旋转图像

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -2,30 +2,24 @@ import java.util.*;
 
 class Solution {
 
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
-        int[] dy = {1, 0, -1, 0};
-        int[] dx = {0, 1, 0, -1};
-        int m = matrix.length, n = matrix[0].length;
-        int x = 0, y = 0, d = 0;
-        for (int i = 0; i < m * n; i++) {
-            res.add(matrix[x][y]);
-            matrix[x][y] = 101;
-            int newX = x + dx[d], newY = y + dy[d];
-            if (newX < 0 || newX >= m || newY < 0 || newY >= n || matrix[newX][newY] == 101) {
-                d = (d + 1) % 4;
-                newX = x + dx[d];
-                newY = y + dy[d];
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int len = n - 1;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < len - i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len - j][i];
+                matrix[len - j][i] =  matrix[len - i][len - j];
+                matrix[len - i][len - j] = matrix[j][len - i];
+                matrix[j][len - i] = temp;
             }
-            x = newX;
-            y = newY;
         }
-        return res;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[][] nums = {{1,2,3}, {4,5,6}, {7,8,9}};
-        System.out.println(s.spiralOrder(nums));
+        int[][] nums = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        s.rotate(nums);
+        System.out.println(Arrays.deepToString(nums));
     }
 }
