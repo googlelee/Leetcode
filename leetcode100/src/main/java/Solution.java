2813fa4756218bfcc44dbce6a9768163dfb6cb1f
DMGK <835306541@qq.com>
2024-03-08 22:21:40 +0800

    leetcode100 240. 搜索二维矩阵 II

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -2,24 +2,49 @@ import java.util.*;
 
 class Solution {
 
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        int len = n - 1;
-        for (int i = 0; i < n / 2; i++) {
-            for (int j = i; j < len - i; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[len - j][i];
-                matrix[len - j][i] =  matrix[len - i][len - j];
-                matrix[len - i][len - j] = matrix[j][len - i];
-                matrix[j][len - i] = temp;
+    // public boolean searchMatrix(int[][] matrix, int target) {
+    //     int top = 0, bottom = matrix.length - 1;
+    //     while (top < bottom) {
+    //         int mid = (top + bottom) / 2;
+    //         if (target < matrix[mid][0]) {
+    //             bottom = mid;
+    //         } else {
+    //             top = mid + 1;
+    //         }
+    //     }
+    //     for (int i = 0; i <= top; i++) {
+    //         int left = 0, right = matrix[0].length - 1;
+    //         while (left < right) {
+    //             int mid = (left + right) / 2;
+    //             if (target > matrix[i][mid]) {
+    //                 left = mid + 1;
+    //             } else {
+    //                 right = mid;
+    //             }
+    //         }
+    //         if (matrix[i][left] == target) return true;
+    //     }
+    //     return false;
+    // }
+
+    // 技巧：右上角开始有规律 一直搜即可
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < matrix.length && j >= 0) {
+            if (target == matrix[i][j]) return true;
+            else if (target > matrix[i][j]) {
+                i++;
+            } else {
+                j--;
             }
         }
+        return false;
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Solution s = new Solution();
-        int[][] nums = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        s.rotate(nums);
-        System.out.println(Arrays.deepToString(nums));
+        int[][] nums = {{-5},{6}};
+        System.out.println(s.searchMatrix(nums, 6));
     }
 }
