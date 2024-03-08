2974e381ac405790c3aba81634127626f1e30dcc
DMGK <835306541@qq.com>
2024-03-08 13:57:20 +0800

    leetcode100 3. 无重复字符的最长子串

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -3,15 +3,16 @@ import java.util.*;
 class Solution {
 
     public int lengthOfLongestSubstring(String s) {
-        int l = 0, r = 0, res = 0;
         if (s == null) return 0;
         if (s.length() < 2) return s.length();
+        int l = 0, r = 0, res = 0;
+        Set<Character> hashSet = new HashSet<>();
         while (r < s.length()) {
-            for (int i = l; i < r; i++) {
-                if (s.charAt(i) == s.charAt(r)) {
-                    l = i + 1;
-                }
+            while (hashSet.contains(s.charAt(r))) {
+                hashSet.remove(s.charAt(l));
+                l++;
             }
+            hashSet.add(s.charAt(r));
             if (res < r - l + 1) res = r - l + 1;
             r++;
         }
