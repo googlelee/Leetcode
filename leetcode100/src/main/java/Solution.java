1af016020a2716f7aad78c84320aef5fe7c62d07
DMGK <835306541@qq.com>
2024-03-08 14:46:13 +0800

    leetcode100 3. 无重复字符的最长子串

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -2,26 +2,39 @@ import java.util.*;
 
 class Solution {
 
-    public int lengthOfLongestSubstring(String s) {
-        if (s == null) return 0;
-        if (s.length() < 2) return s.length();
-        int l = 0, r = 0, res = 0;
-        Set<Character> hashSet = new HashSet<>();
-        while (r < s.length()) {
-            while (hashSet.contains(s.charAt(r))) {
-                hashSet.remove(s.charAt(l));
-                l++;
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        int l = 0;
+        int len = p.length();
+        int end = s.length() - p.length();
+        if (end < 0) return new ArrayList<>();  // 边界
+        int[] pCnt = new int[26];
+        int[] sCnt = new int[26];
+        for (int i = 0; i < len; i++) {
+            pCnt[p.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < len; i++) {
+            sCnt[s.charAt(i) - 'a']++;
+        }
+
+        while (l <= end) {
+            if (Arrays.equals(pCnt, sCnt)) {
+                res.add(l);
             }
-            hashSet.add(s.charAt(r));
-            if (res < r - l + 1) res = r - l + 1;
-            r++;
+            sCnt[s.charAt(l) - 'a']--;
+            if (l + len < s.length()) { // 边界
+                sCnt[s.charAt(l + len) - 'a']++;
+            }
+            l++;
         }
         return res;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String str = "abcabcbb";
-        System.out.println(s.lengthOfLongestSubstring(str));
+        String str = "aaaaaaaaaa";
+        String p = "aaaaaaaaaaaaa";
+        System.out.println(s.findAnagrams(str, p));
     }
 }
