07ec295c6b4a867fbd064839d647f3464ba1dafa
DMGK <835306541@qq.com>
2024-03-08 17:38:01 +0800

    leetcode100 189. 轮转数组

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -2,52 +2,42 @@ import java.util.*;
 
 class Solution {
 
-    public int[][] merge(int[][] intervals) {
-        List<int[]> res = new ArrayList<>();
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                // 首先比较第一个元素
-                int compareFirst = Integer.compare(a[0], b[0]);
-                if (compareFirst != 0) {
-                    return compareFirst;
-                }
-                // 如果第一个元素相同，则比较第二个元素
-                return Integer.compare(a[1], b[1]);
-            }
-        });
+    // public void rotate(int[] nums, int k) {
+    //     int len = nums.length;
+    //     k = k % len;
+    //     int[] tmp = new int[nums.length];
+    //     for (int i = 0; i < len; i++) {
+    //         if (len - k + i < len) {
+    //             tmp[i] = nums[len - k + i];
+    //         } else {
+    //             tmp[i] = nums[(len - k + i) % len];
+    //         }
+    //     }
+    //     System.arraycopy(tmp, 0, nums, 0, len);
+    // }
 
-        int len = intervals.length;
-        int i = 0, j = 0;
-        int start = 0;
-        int end = 0;
-        while (i < len) {
-            start = i;
-            end = intervals[i][1];
-            while (j < len && end >= intervals[j][0]) {
-                end = Math.max(end, intervals[j][1]);
-                j++;
-            }
-            if (j < len) {
-                int[] p = new int[]{intervals[start][0], end};
-                res.add(p);
-            }
-            i = j;
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k = k % len;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-
-        int[] p = new int[]{intervals[start][0], end};
-        res.add(p);
-
-        int[][] ans = new int[res.size()][];
-        for (int k = 0; k < res.size(); k++) {
-            ans[k] = res.get(k);
-        }
-        return ans;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[][] nums = {{2, 3}, {1, 4}};
-        System.out.println(Arrays.deepToString(s.merge(nums)));
+        int[] nums = {1,2,3,4,5,6,7};
+        s.rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
     }
 }
