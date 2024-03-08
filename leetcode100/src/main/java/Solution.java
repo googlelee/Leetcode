b64c649bf253eb33082215f9a84f2a02cfb02b09
DMGK <835306541@qq.com>
2024-03-08 11:04:11 +0800

    leetcode100 49. 字母异位词分组

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -4,20 +4,30 @@ class Solution {
 
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> hashMap = new HashMap<>();
-        for (int i = 0; i < strs.length; i++) {
-            char[] tmp = strs[i].toCharArray();
-            Arrays.sort(tmp);
-            String key = new String(tmp);
-            List<String> ss = hashMap.getOrDefault(key, new ArrayList<>());
-            ss.add(strs[i]);
-            hashMap.put(key, ss);
+        for (String str : strs) {
+            int[] cnt = new int[26];
+            for (int j = 0; j < str.length(); j++) {
+                cnt[str.charAt(j) - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 26; j++) {
+                if (cnt[j] > 0) {
+                    sb.append("#");
+                    char c = (char) ('a' + j);
+                    sb.append(c);
+                    sb.append(cnt[j]);
+                }
+            }
+            List<String> list = hashMap.getOrDefault(sb.toString(), new ArrayList<>());
+            list.add(str);
+            hashMap.put(sb.toString(), list);
         }
         return new ArrayList<>(hashMap.values());
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        String[] strs = {"bdddddddddd","bbbbbbbbbbc"};
         System.out.println(s.groupAnagrams(strs));
     }
 }
