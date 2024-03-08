edd8d4e35fc04fc7569063f2a9fb4e9bfea3525a
DMGK <835306541@qq.com>
2024-03-08 15:56:44 +0800

    leetcode100 560. 和为 K 的子数组 前缀和

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,22 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
 
     public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        int pre = 0;
         int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    res++;
-                    // break; {-1,1,0} 正确为3 break是2
-                }
+        hashMap.put(0, 1);
+        for (int num : nums) {
+            pre += num;
+            if (hashMap.containsKey(pre - k)) {
+                res += hashMap.get(pre - k);
             }
+            hashMap.put(pre, hashMap.getOrDefault(pre, 0) + 1);
         }
         return res;
     }
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {1,1,1};
-        System.out.println(s.subarraySum(nums, 2));
+        int[] nums = {-1,1,0};
+        System.out.println(s.subarraySum(nums, 0));
     }
 }
