94948b3091dda1813dd4b54bf6c80e503b921499
DMGK <835306541@qq.com>
2024-03-11 11:19:36 +0800

    leetcode100 78. 子集 模板

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -2,33 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> item = new ArrayList<>();
-        backtrace(candidates, target, 0, 0, item, res);
+        res.add(new ArrayList<>());
+        backtrace(nums, 0, item, res);
         return res;
     }
 
-    private void backtrace(int[] candidates, int target, int i, int sum, List<Integer> item, List<List<Integer>> res) {
-        if (sum >= target) {
-            if (sum == target) {
-                res.add(new ArrayList<>(item));
-            }
-            return;
-        }
-        for (int j = i; j < candidates.length; j++) {
-            if (candidates[j] > target) continue;
-            item.add(candidates[j]);
-            sum += candidates[j];
-            backtrace(candidates, target, j, sum, item, res);
-            sum -= candidates[j];
+    private void backtrace(int[] nums, int i, List<Integer> item, List<List<Integer>> res) {
+        if (i == nums.length) return;
+        for (int j = i; j < nums.length; j++) {
+            item.add(nums[j]);
+            res.add(new ArrayList<>(item));
+            backtrace(nums, j + 1, item, res);
             item.removeLast();
         }
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {2,3,6,7};
-        System.out.println(s.combinationSum(nums, 7));
+        int[] nums = {1, 2, 3};
+        System.out.println(s.subsets(nums));
     }
 }
