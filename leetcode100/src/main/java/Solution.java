918242f364ec89e50ab0a1a26968bfdedd7fbf02
DMGK <835306541@qq.com>
2024-03-11 20:33:12 +0800

    leetcode100 79. 单词搜索

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,35 +1,41 @@
 class Solution {
-    int max = 0;
-    public int diameterOfBinaryTree(TreeNode root) {
-        getDepth(root);
-        return max;
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                StringBuilder stringBuilder = new StringBuilder();
+                boolean[][] mark = new boolean[board.length][board[0].length];
+                if (backtrace(board, mark, i, j, word, 0, stringBuilder)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-    private int getDepth(TreeNode root) {
-        if (root == null) return 0;
-        int l = getDepth(root.left);
-        int r = getDepth(root.right);
-        if (l + r > max) {
-            max = l + r;
+    private boolean backtrace(char[][] board, boolean[][] mark, int i, int j, String word, int k, StringBuilder stringBuilder) {
+        if (k == word.length() || i >= board.length || j >= board[0].length || i < 0 || j < 0 || stringBuilder.toString().equals(word)) {
+            return stringBuilder.toString().equals(word);
         }
-        return Math.max(l, r) + 1;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        if (!mark[i][j] && board[i][j] == word.charAt(k)) {
+            stringBuilder.append(word.charAt(k));
+            mark[i][j] = true;
+            for (int l = 0; l < 4; l++) {
+                if (backtrace(board, mark, i + dx[l], j + dy[l], word, k+1, stringBuilder)) {
+                    return true;
+                }
+            }
+            mark[i][j] = false;
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-
-        TreeNode a = new TreeNode(1);
-        TreeNode b = new TreeNode(2);
-        TreeNode c = new TreeNode(3);
-        TreeNode d = new TreeNode(4);
-        TreeNode e = new TreeNode(5);
-
-        a.left = b;
-        a.right = c;
-        b.left = d;
-        b.right = e;
-
-        System.out.println(s.diameterOfBinaryTree(a));
+        char[][] board = {{'C', 'A', 'A'}, {'A', 'A', 'A'}, {'B', 'C', 'D'}};
+        System.out.println(s.exist(board, "AAB"));
     }
 }
 
