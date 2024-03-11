f8ebafb286da808d0b2d75104a0d4ace019e7ef2
DMGK <835306541@qq.com>
2024-03-11 21:59:32 +0800

    leetcode100 200. 岛屿数量

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,53 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
 class Solution {
-    String[] str = new String[]{"(", ")"};
-
-    public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-        StringBuilder item = new StringBuilder();
-        backtrace(n, 0, 0, item, res);
-        return res;
-    }
-
-    private void backtrace(int n, int leftLen, int rightLen, StringBuilder item, List<String> res) {
-        if (item.length() >= n * 2 || leftLen > n || rightLen > n || rightLen > leftLen) {
-            if (item.length() == n * 2 && isValid(item)) {
-                res.add(new String(item));
-            }
-            return;
-        }
-        for (String s : str) {
-            item.append(s);
-            if (s.equals("(")) {
-                backtrace(n, leftLen + 1, rightLen, item, res);
-            } else {
-                backtrace(n, leftLen, rightLen + 1, item, res);
-            }
-            item.deleteCharAt(item.length() - 1);
-        }
-    }
-
-    private boolean isValid(StringBuilder item) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < item.length(); i++) {
-            if (stack.isEmpty()) {
-                stack.push(item.charAt(i));
-            } else {
-                if (item.charAt(i) == ')' && stack.peek() == '(') {
-                    stack.pop();
-                } else {
-                    stack.push(item.charAt(i));
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        int m = grid.length, n = grid[0].length;
+        boolean[][] mark = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!mark[i][j] && grid[i][j] == '1') {
+                    backtrace(grid, mark, i, j);
+                    res++;
                 }
             }
         }
-        return stack.isEmpty();
+        return res;
+    }
+
+    private void backtrace(char[][] grid, boolean[][] mark, int i, int j) {
+        mark[i][j] = true;
+        int[] di = {0, 1, 0, -1};
+        int[] dj = {1, 0, -1, 0};
+        for (int k = 0; k < 4; k++) {
+            int newI = i + di[k];
+            int newJ = j + dj[k];
+            if (newI >= 0 && newI < grid.length && newJ >= 0 && newJ < grid[0].length && grid[newI][newJ] == '1' && !mark[newI][newJ]) {
+                backtrace(grid, mark, newI, newJ);
+            }
+        }
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.generateParenthesis(4));
+        char[][] grid = {{'1','1','1','1','0'}, {'1','1','0','1','0'}, {'1','1','0','0','0'}, {'0','0','0','0','0'}};
+        System.out.println(s.numIslands(grid));
     }
 }
