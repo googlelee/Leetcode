97a3e6de7b2466d6a3f5c351807b294d00ed39d6
DMGK <835306541@qq.com>
2024-03-11 10:24:20 +0800

    leetcode100 78. 子集 位运算

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,86 +1,25 @@
-import javax.xml.stream.FactoryConfigurationError;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
-    // 中心扩散
-    public String longestPalindrome(String s) {
-        int maxLen = 0;
-        int start = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int[] odd = palindromeList(s, i, i);
-            int[] even = palindromeList(s, i, i + 1);
-            if (even[1] > odd[1]) {
-                if (maxLen < even[1]) {
-                    maxLen = even[1];
-                    start = even[0];
-                }
-            } else {
-                if (maxLen < odd[1]) {
-                    maxLen = odd[1];
-                    start = odd[0];
-                }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = 1 << nums.length;
+        for (int i = 0; i < n; i++) {
+            List<Integer> item = new ArrayList<>();
+            for (int j = 0; j < nums.length; j++) {
+                int m = 1 << j;
+                if ((i & m) != 0) item.add(nums[j]);
             }
+            res.add(item);
         }
-        return s.substring(start, start + maxLen);
+        return res;
     }
-
-    private int[] palindromeList(String s, int c1, int c2) {
-        int left = c1, right = c2;
-        while (left >= 0 && right < s.length()) {
-            if (s.charAt(left) == s.charAt(right)) {
-                left--;
-                right++;
-            } else break;
-        }
-        return new int[]{left + 1, right - 1 - left};
-    }
-
-
-    // 动态规划：转移方程当前的状态取决于左下角状态 所以填充顺序要考虑
-    // public String longestPalindrome(String s) {
-    //     boolean[][] dp = new boolean[s.length()][s.length()];
-    //     if (s.length() == 1) return s; // 边界 a
-    //     int max = 0;
-    //     int start = 0;
-    //     int end = 1; // 边界 ab
-    //     for (int j = 1; j < s.length(); j++) {
-    //         for (int i = 0; i < j; i++) {
-    //             boolean equal = s.charAt(i) == s.charAt(j);
-    //             if (j - i < 3) dp[i][j] = equal;
-    //             else dp[i][j] = dp[i + 1][j - 1] && equal;
-    //             if (dp[i][j] && max < j - i) {
-    //                 max = j - i;
-    //                 start = i;
-    //                 end = j + 1;
-    //             }
-    //         }
-    //     }
-    //     return s.substring(start, end);
-    // }
-
 
     public static void main(String[] args) {
-
         Solution s = new Solution();
-        String str = "ab";
-        System.out.println(s.longestPalindrome(str));
-    }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        int[] nums = {1, 2, 3};
+        System.out.println(s.subsets(nums));
     }
 }
