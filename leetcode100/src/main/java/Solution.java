9b46e68eafea365e960a5e1fc1593e4b5ef3b723
DMGK <835306541@qq.com>
2024-03-11 16:38:09 +0800

    leetcode100 543. 二叉树的直径

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,40 +1,53 @@
-import java.lang.runtime.TemplateRuntime;
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public Node copyRandomList(Node head) {
-        Map<Node, Node> hashMap = new HashMap<>();
-        Node tmp = head;
-        while (tmp != null) {
-            hashMap.put(tmp, new Node(tmp.val));
-            tmp = tmp.next;
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        getDepth(root);
+        return max;
+    }
+
+    private int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        int l = getDepth(root.left);
+        int r = getDepth(root.right);
+        if (l + r > max) {
+            max = l + r;
         }
-        tmp = head;
-        while (tmp != null) {
-            hashMap.get(tmp).next = hashMap.get(tmp.next);
-            if (tmp.random != null) {
-                hashMap.get(tmp).random = hashMap.get(tmp.random);
-            }
-            tmp = tmp.next;
-        }
-        return hashMap.get(head);
+        return Math.max(l, r) + 1;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.copyRandomList(3));
+
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        TreeNode e = new TreeNode(5);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+
+        System.out.println(s.diameterOfBinaryTree(a));
     }
 }
 
-class Node {
+class TreeNode {
     int val;
-    Node next;
-    Node random;
+    TreeNode left;
+    TreeNode right;
 
-    public Node(int val) {
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
         this.val = val;
-        this.next = null;
-        this.random = null;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
