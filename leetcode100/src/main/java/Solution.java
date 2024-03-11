e56df621fe0dd55c6454a3bb6401f5a88e9fb0fb
DMGK <835306541@qq.com>
2024-03-11 21:13:42 +0800

    leetcode100 22. 括号生成 剪枝

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -8,20 +8,24 @@ class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
         StringBuilder item = new StringBuilder();
-        backtrace(n * 2, item, res);
+        backtrace(n, 0, 0, item, res);
         return res;
     }
 
-    private void backtrace(int len, StringBuilder item, List<String> res) {
-        if (item.length() >= len) {
-            if (item.length() == len && isValid(item)) {
+    private void backtrace(int n, int leftLen, int rightLen, StringBuilder item, List<String> res) {
+        if (item.length() >= n * 2 || leftLen > n || rightLen > n || rightLen > leftLen) {
+            if (item.length() == n * 2 && isValid(item)) {
                 res.add(new String(item));
             }
             return;
         }
         for (String s : str) {
             item.append(s);
-            backtrace(len, item, res);
+            if (s.equals("(")) {
+                backtrace(n, leftLen + 1, rightLen, item, res);
+            } else {
+                backtrace(n, leftLen, rightLen + 1, item, res);
+            }
             item.deleteCharAt(item.length() - 1);
         }
     }
@@ -44,25 +48,6 @@ class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.generateParenthesis(3));
-    }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        System.out.println(s.generateParenthesis(4));
     }
 }
