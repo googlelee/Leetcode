4072a555815e6f486d2aad83a0b619bc059e116e
DMGK <835306541@qq.com>
2024-03-11 10:30:28 +0800

    leetcode100 46. 全排列

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -3,23 +3,34 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        int n = 1 << nums.length;
-        for (int i = 0; i < n; i++) {
-            List<Integer> item = new ArrayList<>();
-            for (int j = 0; j < nums.length; j++) {
-                int m = 1 << j;
-                if ((i & m) != 0) item.add(nums[j]);
-            }
-            res.add(item);
-        }
+        List<Integer> item = new ArrayList<>();
+        int[] mark = new int[nums.length];
+        dfs(nums, 0, item, mark, res);
         return res;
+    }
+
+    private void dfs(int[] nums, int i, List<Integer> item, int[] mark, List<List<Integer>> res) {
+        if (i == nums.length) {
+            // 新对象保存
+            res.add(new ArrayList<>(item));
+            return;
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (mark[j] == 0) {
+                item.add(nums[j]);
+                mark[j] = 1;
+                dfs(nums, i + 1, item, mark, res);
+                mark[j] = 0;
+                item.removeLast();
+            }
+        }
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums = {1, 2, 3};
-        System.out.println(s.subsets(nums));
+        System.out.println(s.permute(nums));
     }
 }
