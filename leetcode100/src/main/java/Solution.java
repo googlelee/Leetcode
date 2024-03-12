846635ab04b5d128f36d5e2804ba9be9720c080c
DMGK <835306541@qq.com>
2024-03-12 16:55:48 +0800

    leetcode100 994. 腐烂的橘子 BFS

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,35 +1,52 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class Solution {
-    public int numIslands(char[][] grid) {
-        int res = 0;
-        int m = grid.length, n = grid[0].length;
-        boolean[][] mark = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!mark[i][j] && grid[i][j] == '1') {
-                    backtrace(grid, mark, i, j);
-                    res++;
+    public int orangesRotting(int[][] grid) {
+        List<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                    grid[i][j] = -1;
                 }
             }
         }
-        return res;
-    }
-
-    private void backtrace(char[][] grid, boolean[][] mark, int i, int j) {
-        mark[i][j] = true;
-        int[] di = {0, 1, 0, -1};
-        int[] dj = {1, 0, -1, 0};
-        for (int k = 0; k < 4; k++) {
-            int newI = i + di[k];
-            int newJ = j + dj[k];
-            if (newI >= 0 && newI < grid.length && newJ >= 0 && newJ < grid[0].length && grid[newI][newJ] == '1' && !mark[newI][newJ]) {
-                backtrace(grid, mark, newI, newJ);
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        int depth = -1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+            for (int i = 0; i < size; i++) {
+                int[] d = queue.removeFirst();
+                int x = d[0];
+                int y = d[1];
+                for (int j = 0; j < 4; j++) {
+                    int newX = x + dx[j];
+                    int newY = y + dy[j];
+                    if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length && grid[newX][newY] == 1) {
+                        queue.add(new int[]{newX, newY});
+                        grid[newX][newY] = -1;
+                    }
+                }
             }
         }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return Math.max(depth, 0); // {{0}}
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        char[][] grid = {{'1','1','1','1','0'}, {'1','1','0','1','0'}, {'1','1','0','0','0'}, {'0','0','0','0','0'}};
-        System.out.println(s.numIslands(grid));
+        int[][] grid = {{2, 2}, {1, 1}, {0, 0}, {2, 0}};
+        System.out.println(s.orangesRotting(grid));
     }
 }
