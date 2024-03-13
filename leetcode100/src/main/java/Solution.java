13c9ab7e64f0f07ae097e4ed6b4a399e96a18045
DMGK <835306541@qq.com>
2024-03-13 19:38:11 +0800

    leetcode100 437. 路径总和 III

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -3,27 +3,51 @@ import java.util.*;
 
 class Solution {
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
-            hashMap.put(inorder[i], i);
-        }
-        return build(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1, hashMap);
+    int res = 0;
+    public int pathSum(TreeNode root, int targetSum) {
+        Map<TreeNode, List<Long>> hashMap = new HashMap<>();
+        dfs(root, new ArrayList<>(), targetSum);
+        return res;
     }
 
-    private TreeNode build(int[] preorder, int[] inorder, int pl, int pr, int il, int ir, Map<Integer, Integer> hashMap) {
-        if (pl > pr) return null;
-        TreeNode root = new TreeNode(preorder[pl]);
-        int rootPos = hashMap.get(root.val);
-        root.left = build(preorder, inorder, pl + 1, pl + rootPos - il, il, rootPos - 1, hashMap);
-        root.right = build(preorder, inorder, pl + rootPos - il + 1, pr, rootPos + 1, ir, hashMap);
-        return root;
+    private void dfs(TreeNode root, List<Long> nums, int targetSum) {
+        if (root == null) return;
+        for (int i = 0; i < nums.size(); i++) {
+            nums.set(i, nums.get(i) + root.val);
+            if (nums.get(i) == targetSum)
+                res++;
+        }
+        nums.add(Long.valueOf(root.val));
+        if (root.val == targetSum)
+            res++;
+        dfs(root.left, new ArrayList<>(nums), targetSum);
+        dfs(root.right, new ArrayList<>(nums), targetSum);
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] preorder = {3, 9, 20, 15, 7};
-        int[] inorder = {9, 3, 15, 20, 7};
-        System.out.println(s.buildTree(preorder, inorder));
+
+        TreeNode a = new TreeNode(10);
+        TreeNode b = new TreeNode(5);
+        TreeNode c = new TreeNode(-3);
+        TreeNode d = new TreeNode(3);
+        TreeNode e = new TreeNode(2);
+        TreeNode f = new TreeNode(11);
+        TreeNode g = new TreeNode(3);
+        TreeNode h = new TreeNode(-2);
+        TreeNode i = new TreeNode(1);
+
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
+        d.left = g;
+        d.right = h;
+        e.right = i;
+
+        s.pathSum(a, 8);
+        System.out.println(s.res);
     }
 }
