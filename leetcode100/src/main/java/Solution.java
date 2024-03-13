97018fc7872939881303372f0e191a0f7ed63fac
DMGK <835306541@qq.com>
2024-03-13 22:35:33 +0800

    leetcode100 763. 划分字母区间

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -5,44 +5,27 @@ import java.util.Map;
 
 class Solution {
 
-    public List<String> letterCombinations(String digits) {
-        Map<Character, char[]> hashMap = new HashMap<>();
-        hashMap.put('2', new char[]{'a', 'b', 'c'});
-        hashMap.put('3', new char[]{'d', 'e', 'f'});
-        hashMap.put('4', new char[]{'g', 'h', 'i'});
-        hashMap.put('5', new char[]{'j', 'k', 'l'});
-        hashMap.put('6', new char[]{'m', 'n', 'o'});
-        hashMap.put('7', new char[]{'p', 'q', 'r', 's'});
-        hashMap.put('8', new char[]{'t', 'u', 'v'});
-        hashMap.put('9', new char[]{'w', 'x', 'y', 'z'});
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> lens = new ArrayList<>();
+        int[] ends = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            ends[s.charAt(i) - 'a'] = i;
+        }
 
-        StringBuilder sb = new StringBuilder();
-        List<String> res = new ArrayList<>();
-        for (int i = 0; i < digits.length(); i++) {
-            char[] chars = hashMap.get(digits.charAt(i));
-            if (res.isEmpty()) {
-                for (int j = 0; j < chars.length; j++) {
-                    res.add(String.valueOf(chars[j]));
-                }
-            } else {
-                int pos = 0;
-                ArrayList<String> tmp = new ArrayList<>();
-                for (int k = 0; k < res.size(); k++) {
-                    String oldStr = res.get(k);
-                    for (int j = 0; j < chars.length; j++) {
-                        String newStr = oldStr + chars[j];
-                        tmp.add(newStr);
-                    }
-                }
-                res = new ArrayList<>(tmp);
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, ends[s.charAt(i) - 'a']);
+            if (i == end) {
+                lens.add(end - start + 1);
+                start = end + 1;
             }
         }
-        return res;
+        return lens;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String digits = "";
-        System.out.println(s.letterCombinations(digits));
+        String digits = "ababcbacadefegdehijhklij";
+        System.out.println(s.partitionLabels(digits));
     }
 }
