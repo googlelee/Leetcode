eba6e61ae016f6575c0152c3b6ac2f14fb67a710
DMGK <835306541@qq.com>
2024-03-13 20:26:17 +0800

    leetcode100 236. 二叉树的最近公共祖先 小心内存超限

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,53 +1,63 @@
-import javax.management.relation.InvalidRoleInfoException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
 
-    int res = 0;
-    public int pathSum(TreeNode root, int targetSum) {
-        Map<TreeNode, List<Long>> hashMap = new HashMap<>();
-        dfs(root, new ArrayList<>(), targetSum);
+    List<TreeNode> path;
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pPath = new ArrayList<>();
+        List<TreeNode> qPath = new ArrayList<>();
+        dfs(root, p, pPath);
+        pPath = path;
+        path = null;
+        dfs(root, q, qPath);
+        qPath = path;
+        TreeNode res = null;
+        for (int i = 0; i < Math.min(pPath.size(), qPath.size()); i++) {
+            if (pPath.get(i) == qPath.get(i)) res = path.get(i);
+        }
         return res;
     }
 
-    private void dfs(TreeNode root, List<Long> nums, int targetSum) {
-        if (root == null) return;
-        for (int i = 0; i < nums.size(); i++) {
-            nums.set(i, nums.get(i) + root.val);
-            if (nums.get(i) == targetSum)
-                res++;
+    private void dfs(TreeNode root, TreeNode target, List<TreeNode> path) {
+        if (root == null || this.path != null) {
+            return;
         }
-        nums.add(Long.valueOf(root.val));
-        if (root.val == targetSum)
-            res++;
-        dfs(root.left, new ArrayList<>(nums), targetSum);
-        dfs(root.right, new ArrayList<>(nums), targetSum);
+        path.add(root);
+        if (root == target) {
+            this.path = new ArrayList<>(path);
+            return;
+        }
+        dfs(root.left, target, path);
+        dfs(root.right, target, path);
+        path.removeLast();
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        TreeNode a = new TreeNode(10);
+        TreeNode a = new TreeNode(3);
         TreeNode b = new TreeNode(5);
-        TreeNode c = new TreeNode(-3);
-        TreeNode d = new TreeNode(3);
+        TreeNode c = new TreeNode(1);
+        TreeNode d = new TreeNode(6);
         TreeNode e = new TreeNode(2);
-        TreeNode f = new TreeNode(11);
-        TreeNode g = new TreeNode(3);
-        TreeNode h = new TreeNode(-2);
-        TreeNode i = new TreeNode(1);
+        TreeNode f = new TreeNode(0);
+        TreeNode g = new TreeNode(8);
+        TreeNode h = new TreeNode(7);
+        TreeNode i = new TreeNode(4);
 
 
         a.left = b;
         a.right = c;
         b.left = d;
         b.right = e;
-        c.right = f;
+        c.left = f;
+        c.right = g;
         d.left = g;
-        d.right = h;
+        e.left = h;
         e.right = i;
 
-        s.pathSum(a, 8);
-        System.out.println(s.res);
+        System.out.println(s.lowestCommonAncestor(a, b, i).val);
     }
 }
