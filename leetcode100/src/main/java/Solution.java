adea5af99485e95dd8d21c72ed7f4b813f52b957
DMGK <835306541@qq.com>
2024-05-31 09:48:05 +0800

    Leetcode 912. 排序数组 冒泡 超时

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,47 +1,24 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-
 class Solution {
 
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode pre = new ListNode(-1);
-        ListNode p = pre;
-        PriorityQueue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
-        for (int i = 0; i < lists.length; i++) {
-            ListNode tmp = lists[i];
-            while (tmp != null) {
-                queue.add(new ListNode(tmp.val)); // 不能直接加入tmp，会成环
-                tmp = tmp.next;
+    public int[] sortArray(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
             }
         }
-        while (!queue.isEmpty()) {
-            p.next = queue.poll();
-            p = p.next;
-        }
-
-        return pre.next;
+        return nums;
     }
 
-
     public static void main(String[] args) {
-        Solution s = new Solution();
-        ListNode a = new ListNode(-2);
-        ListNode b = new ListNode(-1);
-        ListNode c = new ListNode(-1);
-        ListNode d = new ListNode(-1);
-
-        ListNode e = null;
-
-
-        a.next = b;
-        b.next = c;
-        c.next = d;
-
-
-        ListNode[] lists = {a, e};
-
-        System.out.println(s.mergeKLists(lists));
+        int[] nums = {1,2,5,4,3};
+        new Solution().sortArray(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
     }
 }
