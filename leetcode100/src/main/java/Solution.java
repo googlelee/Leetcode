02dab983b49e4409ef2642e9c874770e33d4dbe0
DMGK <835306541@qq.com>
2024-08-16 13:33:44 +0800

    Leetcode 236. 二叉树的最近公共祖先

--- leetcode100/src/main/java/Solution.java
+++ leetcode100/src/main/java/Solution.java
@@ -1,48 +1,53 @@
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<TreeNode> queue = new LinkedList<>();
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
-        queue.add(root);
-        int depth = 0;
-        while (!queue.isEmpty()) {
-            // 最巧妙之处就在于用size控制逐层遍历
-            int size = queue.size();
-            depth += 1;
-            List<Integer> list = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.removeFirst();
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
-                list.add(node.val);
+    // 思路 ： 获取节点路径 最后一个相同的节点就是结果
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pPath = new ArrayList<>();
+        List<TreeNode> path = new ArrayList<>();
+        dfs(root, p, path, pPath);
+        List<TreeNode> qPath = new ArrayList<>();
+        dfs(root, q, path, qPath);
+        TreeNode result = null;
+        for (int i = 0; i < Math.min(pPath.size(), qPath.size()); i++) {
+            if (pPath.get(i) != qPath.get(i)) {
+                return result;
+            } else {
+                result = pPath.get(i);
             }
-            if (depth % 2 == 0) {
-                Collections.reverse(list);
-            }
-            result.add(list);
         }
         return result;
     }
 
-    public static void main(String[] args) {
+    private void dfs(TreeNode root, TreeNode p, List<TreeNode> path, List<TreeNode> ans) {
+        if (root == p) {
+            ans.addAll(path);
+            ans.add(root);
+            return;
+        }
+        path.add(root);
+        if (root.left != null) {
+            dfs(root.left, p, path, ans);
+        }
+        if (root.right != null) {
+            dfs(root.right, p, path, ans);
+        }
+        path.remove(root);
+    }
 
+
+    public static void main(String[] args) {
         Solution s = new Solution();
 
-        TreeNode a = new TreeNode(1);
-        TreeNode b = new TreeNode(2);
-        TreeNode c = new TreeNode(2);
-        TreeNode d = new TreeNode(2);
+        TreeNode a = new TreeNode(3);
+        TreeNode b = new TreeNode(5);
+        TreeNode c = new TreeNode(1);
+        TreeNode d = new TreeNode(6);
         TreeNode e = new TreeNode(2);
-        TreeNode f = new TreeNode(4);
-        TreeNode g = new TreeNode(3);
+        TreeNode f = new TreeNode(0);
+        TreeNode g = new TreeNode(8);
+        TreeNode h = new TreeNode(7);
+        TreeNode i = new TreeNode(4);
 
 
         a.left = b;
@@ -51,7 +56,10 @@ class Solution {
         b.right = e;
         c.left = f;
         c.right = g;
+        d.left = g;
+        e.left = h;
+        e.right = i;
 
-        System.out.println(s.zigzagLevelOrder(a));
+        System.out.println(s.lowestCommonAncestor(a, b, i).val);
     }
 }
